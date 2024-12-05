b2012ec4e2b809e595914e922dd8f4723e1406ae
mannumishra <mannumishra2207@outlook.com>
2024-12-05 12:21:27 +0530

    add chnages

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -5,75 +5,78 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer>
-        <div className="footer-container">
-          <div className="same-col col-1">
-            <div className="heading">
-              <span>Quick Links</span>
-            </div>
-            <ul className='footerlistsame'>
-              <li><Link>Home</Link></li>
-              <li><Link>About-Us</Link></li>
-              <li><Link to={'/testimonial'}>Testimonial</Link></li>
-              <li><Link to={'/contact'}>Contact</Link></li>
-            </ul>
+      <div className="footer-container">
+        <div className="same-col col-1">
+          <div className="heading">
+            <span>Quick Links</span>
           </div>
-          <div className="same-col col-2">
-            <div className="heading">
-              <span>Our Services</span>
-            </div>
-            <ul className='footerlistsame'>
-              {/* <li><Link to={'/flight'}>Flight</Link></li> */}
-              <li><Link to={'/hotel'}>Hotel</Link></li>
-              <li><Link to={'/domestic'}>DOMESTIC TOUR PACKAGES</Link></li>
-              <li><Link to={'/internation'}>INTERNATIONAL TOUR PACKAGES</Link></li>
-              <li><Link to={'/education'}>EDUCATIONAL TOUR PACKAGES</Link></li>
-              <li><Link to={'/seminar'}>CONFERENCE AND SEMINARS</Link></li>
-            </ul>
+          <ul className='footerlistsame'>
+            <li><Link>Home</Link></li>
+            <li><Link>About-Us</Link></li>
+            <li><Link to={'/testimonial'}>Testimonial</Link></li>
+            <li><Link to={'/contact'}>Contact</Link></li>
+          </ul>
+        </div>
+        <div className="same-col col-2">
+          <div className="heading">
+            <span>Our Services</span>
           </div>
-          <div className="same-col col-3">
-            <div className="heading">
-              <span>Get In Touch</span>
+          <ul className='footerlistsame'>
+            {/* <li><Link to={'/flight'}>Flight</Link></li> */}
+            <li><Link to={'/hotel'}>Hotel</Link></li>
+            <li><Link to={'/domestic'}>DOMESTIC TOUR PACKAGES</Link></li>
+            <li><Link to={'/internation'}>INTERNATIONAL TOUR PACKAGES</Link></li>
+            <li><Link to={'/education'}>EDUCATIONAL TOUR PACKAGES</Link></li>
+            <li><Link to={'/seminar'}>CONFERENCE AND SEMINARS</Link></li>
+          </ul>
+        </div>
+        <div className="same-col col-3">
+          <div className="heading">
+            <span>Get In Touch</span>
+          </div>
+          <div className="adress-box footerlistsame">
+            <div className="location adress-same">
+              <i class="ri-map-pin-range-line"></i>
+              <p>Shop No. TC-415, Fourth Floor, R-Tech Capital Mall</p>
             </div>
-            <div className="adress-box footerlistsame">
-              <div className="location adress-same">
-                <i class="ri-map-pin-range-line"></i>
-                <p>2nd Floor, Building -6, Park End, Vikas Marg, opposite Reliance Trends, Preet Vihar, Delhi, 110092</p>
-              </div>
-              <div className="number adress-same">
-              <i class="ri-phone-fill"></i>
-                <a href='tel:+91 8700865337'>8700865337</a>
-              </div>
-              {/* <div className="number adress-same">
+            <div className="mail adress-same">
+              <i className="ri-mail-fill"></i>
+              <a href="mailto:mrandmrsperfecttrips@gmail.com">mrandmrsperfecttrips@gmail.com</a>
+            </div>
+            <div className="number adress-same">
+              <i className="ri-phone-fill"></i>
+              <a href="tel:+919157575749,+918949655261">9157575749 , 8949655261</a>
+            </div>
+
+            {/* <div className="number adress-same">
               <i class="ri-phone-fill"></i>
                 <a href='tel:+91 9926288424'>9926288424</a>
               </div> */}
-              <div className="mail adress-same">
-              <i class="ri-mail-fill"></i>
-                <a href='mailto:  info@tripjar.in'> info@tripjar.in</a>
-              </div>
-              {/* <div className="mail-2 adress-same">
+
+
+            {/* <div className="mail-2 adress-same">
               <i class="ri-mail-fill"></i>
                 <a href="mailto:sales@greenwaysinfra.com">sales@greenwaysinfra.com</a>
               </div> */}
-            </div>
-          </div>
-          <div className="same-col col-4">
-            <div className="heading">
-              <span>Follow On</span>
-            </div>
-            <div className="social-link">
-              <Link target='_blank' to={''}><i class="ri-facebook-box-fill"></i></Link>
-              <Link target='_blank' to={'https://twitter.com/tripjarpvtltd'}><i class="ri-twitter-fill"></i></Link>
-              <Link target='_blank' to={''}><i class="ri-linkedin-box-fill"></i></Link>
-              <Link target='_blank' to={'https://www.instagram.com/tripjar21/'}><i class="ri-instagram-line"></i></Link>
-            </div>
           </div>
         </div>
-        <div className="bottom-footer">
-          <p>© 2024 Greenway Infra. All Right Reseverd.</p>
-          <p>Designed By <a target='_blank' href="https://digiindiasolutions.com/">Digi India Solution</a></p>
+        <div className="same-col col-4">
+          <div className="heading">
+            <span>Follow On</span>
+          </div>
+          <div className="social-link">
+            <Link target='_blank' to={''}><i class="ri-facebook-box-fill"></i></Link>
+            <Link target='_blank' to={'https://twitter.com/tripjarpvtltd'}><i class="ri-twitter-fill"></i></Link>
+            <Link target='_blank' to={''}><i class="ri-linkedin-box-fill"></i></Link>
+            <Link target='_blank' to={'https://www.instagram.com/tripjar21/'}><i class="ri-instagram-line"></i></Link>
+          </div>
         </div>
-      </footer>
+      </div>
+      <div className="bottom-footer">
+        <p>© 2024 Greenway Infra. All Right Reseverd.</p>
+        <p>Designed By <a target='_blank' href="https://digiindiasolutions.com/">Digi India Solution</a></p>
+      </div>
+    </footer>
   )
 }
 
